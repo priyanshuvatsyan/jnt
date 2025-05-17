6d00172aaccfa14360b7ed776437767c313e0d25
Priyanshu Vatsyan <priyanshuvatsyan@gmail.com>
2025-05-17 10:53:10 +0530

    Update rupee icon

--- src/Shop.jsx
+++ src/Shop.jsx
@@ -143,7 +143,7 @@ export default function Shop() {
                                             />
                                         </div>
                                         <h3 className="product-name">{product.name}</h3>
-                                        <p className="product-price">${product.price.toFixed(2)}</p>
+                                        <p className="product-price">₹{product.price.toFixed(2)}</p>
                                         <button 
                                         className='add-to-cart'
                                         onClick={ ()=> addToCart(product)} >
